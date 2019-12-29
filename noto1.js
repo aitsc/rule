aa610a66848506ecf7961a7447b18b5528484ca7
tsc <tscmail@vip.qq.com>
2019-12-29 19:51:52 +0800

    Update and rename noto.js to noto1.js

--- noto1.js
+++ noto1.js
@@ -1,6 +1,12 @@
 var obj = JSON.parse($response.body);
 
-obj.subscriber = {"entitlements": {"pro": {"expires_date": "2099-01-01T09:26:54Z","product_identifier": "com.lkzhao.editor.pro.ios.yearly","purchase_date": "2019-12-19T09:26:54Z"}};
+obj.subscriber.entitlements = {
+   "pro": {
+    "expires_date": "2099-01-01T09:26:54Z",
+    "product_identifier": "com.lkzhao.editor.pro.ios.yearly",
+    "purchase_date": "2019-12-19T09:26:54Z"
+   }
+};
 
 obj.original_application_version = "31";
 
