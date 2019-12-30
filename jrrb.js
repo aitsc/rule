45f760ff8b2c8ee03e9ecba049fbb3cedb5b4f1b
tsc <tscmail@vip.qq.com>
2019-12-30 15:02:06 +0800

    Create jrrb.js

---
+++ jrrb.js
@@ -0,0 +1,18 @@
+/*
+转载：https://raw.githubusercontent.com/maxduke/Rules/master/Surge/Script/jrrb.js
+今日热榜
+http-response https:\/\/api\.tophub\.today\/account\/sync
+hostname= api.tophub.today
+*/
+
+var obj = JSON.parse($response.body);
+ obj={
+ "data": {
+  "is_vip": "1",
+  "vip_expired": "4092599349",
+  "is_vip_now": true
+ },
+ "error": false,
+ "status": 200
+};
+$done({body: JSON.stringify(obj)});
