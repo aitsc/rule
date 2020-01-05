521a5694d1898d61150a8adfb3785f03f86eb7c0
tsc <tscmail@vip.qq.com>
2020-01-05 15:40:26 +0800

    Update smqnw.js

--- smqnw.js
+++ smqnw.js
@@ -11,5 +11,5 @@ QX & Q & Surge Mitm = ap*.intsig.net,
 */
 
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"1643731200"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":"2643731200"}}};
 $done({body: JSON.stringify(obj)});
